5e8ce2833da3206c67d21a21b1b763e7ad38356a
tsaskater <parasztragu@gmail.com>
2021-01-09 22:10:42 +0100

    BLADEHQ_ACEL_ERTEKELES tábla létrehozva

--- 1. Táblák Létrehozása/tablak.sql
+++ 1. Táblák Létrehozása/tablak.sql
@@ -7,3 +7,16 @@ CREATE TABLE CEDRIC_CUT_TESTS(
   CONSTRAINT vagasok_length check (Vagasok between 1 and 10000)
 );
 
+CREATE TABLE BLADEHQ_ACEL_ERTEKELES(
+  Id INT GENERATED ALWAYS as IDENTITY(START with 1 INCREMENT by 1) PRIMARY KEY,
+  Acel VARCHAR(20),
+  Eltartas INT NOT NULL,
+  CONSTRAINT eltartas_rating check (Eltartas between 1 and 10),
+  Erosseg INT NOT NULL,
+  CONSTRAINT erosseg_rating check (Erosseg between 1 and 10),
+  Rozsdamentesseg INT NOT NULL,
+  CONSTRAINT rozsdamentesseg_rating check (Rozsdamentesseg between 1 and 10),
+  Elezes_Konnyedsege INT NOT NULL,
+  CONSTRAINT Elezes_Konnyedsege_rating check (Elezes_Konnyedsege between 1 and 10)
+);
+
